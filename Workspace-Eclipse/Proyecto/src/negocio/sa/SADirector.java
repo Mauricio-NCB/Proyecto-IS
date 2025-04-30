bf5e2d68d3ad322d55315a48e1df63d33f854076
Deiviss <99610648+Lizzen@users.noreply.github.com>
2025-05-01 01:09:24 +0200

    SADirector

--- Workspace-Eclipse/Proyecto/src/negocio/sa/SADirector.java
+++ Workspace-Eclipse/Proyecto/src/negocio/sa/SADirector.java
@@ -1,7 +1,12 @@
 package negocio.sa;
 
 import negocio.dto.TDirector;
+import negocio.dto.TEmpleado;
 
 public interface SADirector {
     boolean altaDirector(TDirector director);
+    public void agregarEmpleadoADirector(String idDirector, TEmpleado empleado);
+    public boolean eliminarDirector(String id);
+    public boolean actualizaDatosDirector(String id, Float sueldo, String contrasena);
+    public void mostrarDirectores();
 }
--- Workspace-Eclipse/Proyecto/src/negocio/sa/SADirectorImp.java
+++ Workspace-Eclipse/Proyecto/src/negocio/sa/SADirectorImp.java
@@ -1,14 +1,64 @@
 package negocio.sa;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 import integracion.DAODirector;
 import integracion.DAODirectorImp;
+import integracion.DAOEmpleado;
+import integracion.DAOEmpleadoImp;
 import negocio.dto.TDirector;
+import negocio.dto.TEmpleado;
+import util.HashUtil;
+import integracion.BDConexion;
 
 public class SADirectorImp implements SADirector {
     private DAODirector daoDirector = new DAODirectorImp();
 
     @Override
     public boolean altaDirector(TDirector director) {
+        /*if (director == null || director.getIdentificador() == null || director.getIdentificador().trim().isEmpty()) {
+            throw new Exception("Datos del director inválidos o identificador vacío.");
+        }
+        if (director.getCargo() == null || director.getCargo().trim().isEmpty()){
+            throw new Exception("El cargo del director no puede ser vacío.");
+        }
+        
+        TDirector existe = daoDirector.obtenerPorId(director.getIdentificador());
+		if (existe != null) {
+		    throw new Exception("Ya existe un director con el identificador: " + director.getIdentificador());
+		}*/
+        
         return daoDirector.createDirector(director);
+    }
+    
+    
+    public void agregarEmpleadoADirector(String idDirector, TEmpleado empleado) {
+        TDirector director = daoDirector.obtenerPorId(idDirector);
+        if (director != null) throw new IllegalArgumentException("Director existe");
+
+        daoDirector.createDirector(new TDirector(empleado.getIdentificador(), empleado.getNombre(), empleado.getSueldo(),
+        		empleado.getContrasena(), "Director"));
+    }
+
+    public boolean eliminarDirector(String id) {
+    	return daoDirector.eliminar(id);
+    }
+    
+    public boolean actualizaDatosDirector(String id, Float sueldo, String contrasena) {
+    	TDirector director = daoDirector.obtenerPorId(id);
+    	if (sueldo != null) {director.setSueldo(sueldo);}
+    	if (!contrasena.trim().isEmpty()) {director.setContrasena(HashUtil.hashPassword(contrasena));}
+    	return daoDirector.actualizar(director);
+    }
+    
+    public void mostrarDirectores() {
+    	System.out.println("\n--- Listado de Directores ---");
+    	List<TDirector> directores = daoDirector.obtenerTodos();
+    	
+        for (TDirector director : directores) {
+            System.out.println(director);
+        }    	
     }
 }
